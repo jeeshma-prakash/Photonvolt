1d5649fbc92bf346761e76fdbe9667dc57230cc2
jeeshma prakash s <sjeeshmaprakash@gmail.com>
2025-06-14 20:13:45 +0530

    COMMIT

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,3 +173,23 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       }
     });
+
+
+      const counters = document.querySelectorAll('.num');
+
+    counters.forEach(counter => {
+      const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / 100;
+
+        if (count < target) {
+          counter.innerText = Math.ceil(count + increment);
+          setTimeout(updateCount, 20);
+        } else {
+          counter.innerText = target + (target >= 100 ? '+' : '');
+        }
+      };
+
+      updateCount();
+    });
